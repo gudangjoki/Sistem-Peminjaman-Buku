f7504f70402ccf3f25221a8c31c28064daf85700
unknown <komangweda360@gmail.com>
2024-07-06 21:08:55 +0800

    fix

--- routes/web.php
+++ routes/web.php
@@ -75,7 +75,7 @@ Route::post('/edit-category/{category_id}', [AdminController::class, 'edit_categ
 
 Route::get('/dashboard/{section}', [AdminController::class, 'dashboard']);
 Route::get('/dashboard/denda/all', [AdminController::class, 'view_book_warning']);
-Route::put('/update/denda/', [RentLogsController::class, 'update_status_denda']);
+Route::put('/update/denda/{username}', [RentLogsController::class, 'update_status_denda']);
 
 Route::get('/dashboard/buku/edit-buku/{book_code}', [AdminController::class, 'index_update_book']);
 Route::get('/dashboard/buku/tambah-buku', [ComponentController::class, 'index_add_book']);
